55bce2bfbae7c687c665c7f73a0820b76cb41eba
Trevor Gokey <trevorgokey@gmail.com>
2020-07-29 21:29:09 -0700

    bugfixes; add name param to vina.py

--- docking/vina.py
+++ docking/vina.py
@@ -53,7 +53,7 @@ def to_pdb(in_fnm, out_fnm):
     call(obabel, verbose=False)
 
 
-def dock_structure(prot_fnm, lig_fnm, center, size, log=None):
+def dock_structure(prot_fnm, lig_fnm, center, size, log=None, n_confs=1):
     config = """
     receptor = {0:s}
     ligand = {1:s}
@@ -70,7 +70,7 @@ def dock_structure(prot_fnm, lig_fnm, center, size, log=None):
     size_z = {8:f}
 
     exhaustiveness = 8
-    num_modes = 20
+    num_modes = {10:d}
 
     energy_range = 100
     cpu = 1
@@ -79,7 +79,7 @@ def dock_structure(prot_fnm, lig_fnm, center, size, log=None):
         log = "log"
     vina = "vina --config /dev/stdin"
     out = log.rsplit('.',1)[0] + '.pdbqt'
-    inp = config.format(prot_fnm, lig_fnm, log, *center, *size, out)
+    inp = config.format(prot_fnm, lig_fnm, log, *center, *size, out, n_confs)
     
 
     #breakpoint()
@@ -88,7 +88,7 @@ def dock_structure(prot_fnm, lig_fnm, center, size, log=None):
     print("Wrote", out, log)
 
 
-def run_single(i, c, parm_fnm, traj_fnm, lig_fnm, center, size):
+def run_single(i, c, parm_fnm, traj_fnm, lig_fnm, center, size, n_confs=20, name="site"):
 
     lig_name = lig_fnm.rsplit('.',1)[0]
     
@@ -104,10 +104,10 @@ def run_single(i, c, parm_fnm, traj_fnm, lig_fnm, center, size):
     dockout = "\rDocking center {:d} {:8.6} {:8.6} {:8.6}, frame {:7d}/{:-8d}"
     print(dockout.format(c, *center,i, i), end="")
     #print("\r{:80s}".format(""), end='')
-    log_fnm = pdb_fnm.rsplit('.', 1)[0]+'.c{:d}.{:s}.log'.format(c, lig_name)
-    dock_structure(pdbqt_fnm, lig_fnm, center, size, log=log_fnm)
+    log_fnm = pdb_fnm.rsplit('.', 1)[0]+'.{:s}.{:s}.log'.format(name, lig_name)
+    dock_structure(pdbqt_fnm, lig_fnm, center, size, log=log_fnm, n_confs=n_confs)
 
-    out_fnm = pdb_fnm.rsplit('.', 1)[0]+'.c{:d}.{:s}.pdbqt'.format(c, lig_name)
+    out_fnm = pdb_fnm.rsplit('.', 1)[0]+'.{:s}.{:s}.pdbqt'.format(name, lig_name)
     out_as_pdb_fnm = pdb_fnm.rsplit('.', 1)[0] \
         +'.c{:d}.{:s}.pdb'.format(c, lig_name)
     to_pdb(out_fnm, out_as_pdb_fnm)
@@ -145,29 +145,32 @@ def main():
         description="""
         Dock a compound to an trajectory using OBABEL/CPPTRAJ/VINA""")
 
-    parser.add_argument("--cx", "--center-x", type=float,
-        description="x coordinate of the binding grid origin")
-    parser.add_argument("--cy", "--center-y", type=float,
-        description="y coordinate of the binding grid origin")
-    parser.add_argument("--cz", "--center-z", type=float,
-        description="z coordinate of the binding grid origin")
+    parser.add_argument("-x", "--center-x", type=float,
+        help="x coordinate of the binding grid origin")
+    parser.add_argument("-y", "--center-y", type=float,
+        help="y coordinate of the binding grid origin")
+    parser.add_argument("-z", "--center-z", type=float,
+        help="z coordinate of the binding grid origin")
 
-    parser.add_argument("--lx", "--length-x", type=float,
-        description="x coordinate of the binding grid origin")
-    parser.add_argument("--ly", "--length-y", type=float,
-        description="y coordinate of the binding grid origin")
-    parser.add_argument("--lz", "--length-z", type=float,
-        description="z coordinate of the binding grid origin")
+    parser.add_argument("-a", "--length-x", type=float,
+        help="x coordinate of the binding grid origin")
+    parser.add_argument("-b", "--length-y", type=float,
+        help="y coordinate of the binding grid origin")
+    parser.add_argument("-c", "--length-z", type=float,
+        help="z coordinate of the binding grid origin")
 
     parser.add_argument("-p", "--params", type=str,
-        description="Parm7 file to give to cpptraj")
-    parser.add_argument("-y", "--trajectory", type=str,
-        description="Trajectory to load")
+        help="Parm7 file to give to cpptraj")
+    parser.add_argument("-t", "--trajectory", type=str,
+        help="Trajectory to load")
     parser.add_argument("-l", "--ligand", type=str,
-        description="Ligand file in PDBQT format")
+        help="Ligand file in PDBQT format")
 
-    parser.add_argument("-f", "--frame", type=str,
-        description="Frame to extract from trajectory (1-index)")
+    parser.add_argument("-s", "--site-name", type=str,
+        help="Name of site to uniquify the output files")
+
+    parser.add_argument("-f", "--frame", type=int,
+        help="Frame to extract from trajectory (1-index)")
 
     args = parser.parse_args()
 
@@ -177,22 +180,7 @@ def main():
     center = [args.center_x, args.center_y, args.center_z]
     lens = [args.length_x, args.length_y, args.length_z]
 
-    run_single(args.frame, args.frame, parm, traj, lig, center, lens)
-
-    # centers=[
-    #     [48.86,124.23,120.34],
-    #     [146.86,132.23,120.34],
-    #     [104.86,45.23,120.34],
-    #     [100.19,100.56,130.34]
-    # ]
-    # size=[50,50,50]
-    # frame_start = int(sys.argv[1])
-    # frames = int(sys.argv[2])
-
-
-    # parm_fnm = "top.noh.pdb"
-    # traj_fnm = "traj.noh.pdb"
-    # lig_fnm  = "lig2h.am1.pdbqt"
+    run_single(args.frame, args.frame, parm, traj, lig, center, lens, name=args.site_name)
 
 if __name__ == "__main__":
     main()
